ce47cf96222ad92c2a7617b3afe31423663208a1
white <122345776@umail.ucc.ie>
2025-01-15 15:15:24 +0000

    Created signup and login routes and forms for both, also created 2 decorators for vertical access control

--- core/app.py
+++ core/app.py
@@ -1,7 +1,33 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, session, request, url_for, redirect, g
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__, template_folder="../ui/templates/")
 app.config["SECRET_KEY"] = "j9573-4952-9029-1034"
+
+@app.before_request
+def logged_in_user():
+    g.user = session.get("username", None)
+    g.admin = session.get("username", None)
+
+def login_required(view):
+    """add at start of routes where users need to be logged in to access"""
+    @wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for("login", next=request.url))
+        return view(*args, **kwargs)
+    return wrapped_view
+
+def admin_required(view):
+    """add at start of routes where users admin needs to be logged in to access"""
+    @wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if g.admin != "admin":
+            return redirect(url_for("login", next=request.url))
+        return view(*args, **kwargs)
+    return wrapped_view
 
 @app.route('/')
 def index():
@@ -12,6 +38,30 @@ def index():
     """
 
     return render_template('index.html')
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        # Retrieve data from the sign up form
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        password2 = form.password2.data
+
+        # Store in database
+    return
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Retrieve data from the login form
+        username = form.username.data
+        password = form.username.data
+
+        # Compare with database
+    return
 
 if __name__ == '__main__':
     app.run(debug=True)
