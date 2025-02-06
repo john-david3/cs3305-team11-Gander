3c18421d2b44acf2a013c60048402129603dc081
JustIceO7 <oscarcao777@gmail.com>
2025-02-06 02:59:21 +0000

    FEATURE: Created files for admin routes and utils

---
+++ web_server/blueprints/admin.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from blueprints.utils import admin_required
+
+admin_bp = Blueprint("admin", __name__)
+
+@admin_required
+@admin_bp.route('admin/delete_user/<int:user_id>')
+def admin_delete_user(user_id):
+    return
