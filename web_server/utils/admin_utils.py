3f95b35accc1320ad3365c090b1807e72d1d45ba
JustIceO7 <oscarcao777@gmail.com>
2025-02-27 00:37:19 +0000

    CHANGE: Modified admin utils to use with clause

--- web_server/utils/admin_utils.py
+++ web_server/utils/admin_utils.py
@@ -1,39 +1,38 @@
 from database.database import Database
 
 def check_if_admin(username):
-    # Create a connection to the database
-    db = Database()
-    db.create_connection()
-
-    is_admin = db.fetchone("""
-                SELECT is_admin
-                FROM users
-                WHERE username = ?;
-                """, (username,))
-    
-    return is_admin
+    """
+   Returns whether user is admin 
+    """
+    with Database() as db:
+        is_admin = db.fetchone("""
+                    SELECT is_admin
+                    FROM users
+                    WHERE username = ?;
+                    """, (username,))
+        
+        return bool(is_admin)
 
 def check_if_user_exists(banned_user):
-    # Create a connection to the database
-    db = Database()
-    db.create_connection()
-
-    user_exists = db.fetchone("""
-                SELECT user_id 
-                FROM users
-                WHERE username = ?;""",
-            (banned_user,))
-    
-    return user_exists
+    """
+    Returns whether user exists
+    """
+    with Database() as db:
+        user_exists = db.fetchone("""
+                    SELECT user_id 
+                    FROM users
+                    WHERE username = ?;""",
+                (banned_user,))
+        
+        return bool(user_exists)
 
 def ban_user(banned_user):
-    """Ban a user."""
-    # Create a connection to the database
-    db = Database()
-    db.create_connection()
-
-    db.execute("""
-                DELETE FROM users
-                WHERE username = ?;""", 
-                (banned_user)
-            )
+    """
+    Bans a user
+    """
+    with Database() as db:
+        db.execute("""
+                    DELETE FROM users
+                    WHERE username = ?;""", 
+                    (banned_user)
+                )
